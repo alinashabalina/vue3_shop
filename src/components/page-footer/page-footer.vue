c61840ccbc874377c3f99ddd61879b8898173c27
Alina Shabalina <makosei@list.ru>
2022-07-16 21:35:41 +0300

    Review fix saturday online positioning fixes

--- src/components/page-footer/page-footer.vue
+++ src/components/page-footer/page-footer.vue
@@ -1,24 +1,7 @@
 <template>
   <footer class="footer">
-    <div class="footer__wrapper footer__wrapper--startaligned">
+    <div class="footer__wrapper">
       <img class="footer__logo" :src="require('@/assets/-e-logo-y.svg')">
-      <address class="footer__address">г. Москва, ул. Строителей, 15</address>
-      <img class="footer__rectangle" :src="require('@/assets/Rectangle 15.svg')">
-    </div>
-    <div class="footer__wrapper footer__wrapper--centeraligned">
-      <div class="footer__missing-element"></div>
-      <div class="footer__menu-main">
-        <button class="footer__menu-main-item">Доставка</button>
-        <button class="footer__menu-main-item">Гарантия</button>
-        <button class="footer__menu-main-item">Контакты</button>
-      </div>
-      <div class="footer__socials">
-        <img class="footer__socials-icon" :src="require('@/assets/icon_witter.svg')">
-        <img class="footer__socials-icon" :src="require('@/assets/icon_book.svg')">
-        <img class="footer__socials-icon" :src="require('@/assets/icon_gram.svg')">
-      </div>
-    </div>
-    <div class="footer__wrapper footer__wrapper--endaligned">
       <div class="footer__menu-secondary">
         <div class="footer__menu-secondary-item footer__menu-secondary-item--noauth">
           <MenuButton title="Имя Фамилия" icon="icon_user" mode="light" />
@@ -33,7 +16,25 @@
           <MenuButton title="Корзина" icon="icon_cart" mode="light" />
         </div>
       </div>
+    </div>
+    <div class="footer__wrapper footer__wrapper--spacebetween footer__wrapper--secondrow">
+      <address class="footer__address">г. Москва, ул. Строителей, 15</address>
+      <div class="footer__menu-main">
+        <button class="footer__menu-main-item">Доставка</button>
+        <button class="footer__menu-main-item">Гарантия</button>
+        <button class="footer__menu-main-item">Контакты</button>
+      </div>
       <address class="footer__tel">Тел.: +7 (495) 495-95-95</address>
+    </div>
+
+    <div class="footer__wrapper footer__wrapper--spacebetween">
+      <img class="footer__rectangle" :src="require('@/assets/Rectangle 15.svg')">
+
+      <div class="footer__socials">
+        <img class="footer__socials-icon" :src="require('@/assets/icon_witter.svg')">
+        <img class="footer__socials-icon" :src="require('@/assets/icon_book.svg')">
+        <img class="footer__socials-icon" :src="require('@/assets/icon_gram.svg')">
+      </div>
       <img class="footer__logo-html" :src="require('@/assets/logo-html.svg')">
     </div>
   </footer>
--- src/components/page-header/page-header.vue
+++ src/components/page-header/page-header.vue
@@ -2,35 +2,34 @@
   <header class="header">
     <div class="header__rectangle">
       <img class="header__rectangle-logo" :src="require('@/assets/-e-logo.svg')">
-      <HeaderSearch class="header__search-field" :icon="require('@/assets/icon_search.svg')"></HeaderSearch>
-      <div class="header__rectangle-menu">
-        <div class="header__rectangle-menu-item header__rectangle-menu-item--noauth">
-          <MenuButton title="Имя Фамилия" icon="icon_user" mode="dark" />
-        </div>
-        <div class="header__rectangle-menu-item">
-          <MenuButton title="Войти" icon="icon_user" mode="dark" />
-        </div>
-        <div class="header__rectangle-menu-spring12" />
-        <div class="header__rectangle-menu-item">
-          <MenuButton title="Сравнить" icon="icon_chart" mode="dark" />
-        </div>
-        <div class="header__rectangle-menu-spring23" />
-        <div class="header__rectangle-menu-item">
-          <MenuButton title="Корзина" icon="icon_cart" mode="dark" />
+      <div class="header__rectangle-wrapper">
+        <HeaderSearch class="header__search-field" :icon="require('@/assets/icon_search.svg')"></HeaderSearch>
+        <div class="header__rectangle-menu">
+          <div class="header__rectangle-menu-item header__rectangle-menu-item--noauth">
+            <MenuButton title="Имя Фамилия" icon="icon_user" mode="dark" />
+          </div>
+          <div class="header__rectangle-menu-item">
+            <MenuButton title="Войти" icon="icon_user" mode="dark" />
+          </div>
+          <div class="header__rectangle-menu-item header__rectangle-menu-item--compare">
+            <MenuButton title="Сравнить" icon="icon_chart" mode="dark" />
+          </div>
+          <div class="header__rectangle-menu-item">
+            <MenuButton title="Корзина" icon="icon_cart" mode="dark" />
+          </div>
         </div>
       </div>
+
       <div class="header__rectangle-secondarymenu">
-        <div class="header__rectangle-secondarymenu-item">
+        <div class="header__rectangle-secondarymenu-item header__rectangle-secondarymenu-item--choose">
           <button>Каталог товаров<img class="header__rectangle-secondarymenu-item-pic"
               :src="require('@/assets/icon_plus.svg')" />
           </button>
         </div>
-        <div class="header__rectangle-secondarymenu-spring12"></div>
         <button class="header__rectangle-secondarymenu-item">Доставка</button>
-        <div class="header__rectangle-secondarymenu-spring23"></div>
-        <button class="header__rectangle-secondarymenu-item">Гарантия</button>
-        <div class="header__rectangle-secondarymenu-spring34"></div>
-        <button class="header__rectangle-secondarymenu-item">Контакты</button>
+        <button
+          class="header__rectangle-secondarymenu-item header__rectangle-secondarymenu-item--warranty">Гарантия</button>
+        <button class="header__rectangle-secondarymenu-item header__rectangle-secondarymenu-item">Контакты</button>
       </div>
     </div>
   </header>
